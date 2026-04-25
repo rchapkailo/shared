818eb402c759d4ffe3ca1e0d5e4e1d5456f66d03
rchapkailo <romafromukraine@gmail.com>
2026-04-25 22:52:42 +0300

    chore: consolidate release workflow and tooling updates

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface RepoInfo {
-	name: string;
-	deps: string[];
-}
-
-export declare const REPO_NAME: string;
-export declare function getRepoInfo(): RepoInfo;
-export declare function getRepoName(): string;
